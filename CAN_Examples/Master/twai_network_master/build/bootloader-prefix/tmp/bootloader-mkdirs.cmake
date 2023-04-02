bc671f5e1463d00a75c2217e13d3e5305f0c80a5
rahgun04 <38143512+rahgun04@users.noreply.github.com>
2023-04-02 11:35:37 +0100

    PCB, Example Code

---
+++ CAN_Examples/Master/twai_network_master/build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,19 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "D:/Users/rahul/esp/esp-idf/components/bootloader/subproject"
+  "D:/Projects/SEM/SEM_Display/CAN_Examples/Master/twai_network_master/build/bootloader"
+  "D:/Projects/SEM/SEM_Display/CAN_Examples/Master/twai_network_master/build/bootloader-prefix"
+  "D:/Projects/SEM/SEM_Display/CAN_Examples/Master/twai_network_master/build/bootloader-prefix/tmp"
+  "D:/Projects/SEM/SEM_Display/CAN_Examples/Master/twai_network_master/build/bootloader-prefix/src/bootloader-stamp"
+  "D:/Projects/SEM/SEM_Display/CAN_Examples/Master/twai_network_master/build/bootloader-prefix/src"
+  "D:/Projects/SEM/SEM_Display/CAN_Examples/Master/twai_network_master/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "D:/Projects/SEM/SEM_Display/CAN_Examples/Master/twai_network_master/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
